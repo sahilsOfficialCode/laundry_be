3fd738a71e8091d6bb9d3bdba1205496816f2ea8
sahil <sahilsajith.official@gmail.com>
2026-07-02 18:57:39 +0530

    otp setup using firebase

---
+++ src/auth/dto/firebase-login.dto.ts
@@ -0,0 +1,20 @@
+import { IsOptional, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class FirebaseLoginDto {
+  @IsString()
+  firebaseIdToken: string;
+
+  @IsString()
+  @MinLength(10)
+  @MaxLength(16)
+  @Matches(/^\+?[0-9]{10,15}$/, {
+    message:
+      'mobileNumber must contain only digits and can optionally start with +',
+  })
+  mobileNumber: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(60)
+  name?: string;
+}
